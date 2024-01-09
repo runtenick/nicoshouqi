bceee7be68bbc2d3fd8671789341bf41bde3293b
Nico <nicolas.franco@etu.uca.fr>
2024-01-09 18:04:38 +0100

    enum CellType

---
+++ model/Sources/model/enums/CellType.swift
@@ -0,0 +1,17 @@
+//
+//  CellType.swift
+//  
+//
+//  Created by étudiant on 09/01/2024.
+//
+
+import Foundation
+
+enum CellType {
+    case unknown
+    case jungle
+    case water
+    case trap
+    case den
+}
+
