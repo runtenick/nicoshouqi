20143a4208d4456ef73e9d8c01943ce1c9002959
Nico <nicolas.franco@etu.uca.fr>
2024-01-09 18:25:17 +0100

    Owner enum + Animal enum

---
+++ model/Sources/model/enums/Animal.swift
@@ -0,0 +1,19 @@
+//
+//  Animal.swift
+//  
+//
+//  Created by étudiant on 09/01/2024.
+//
+
+import Foundation
+
+enum Animal {
+    case rat
+    case cat
+    case dog
+    case wolf
+    case leopard
+    case tiger
+    case lion
+    case elephant
+}
---
+++ model/Sources/model/enums/Owner.swift
@@ -0,0 +1,14 @@
+//
+//  Owner.swift
+//  
+//
+//  Created by étudiant on 09/01/2024.
+//
+
+import Foundation
+
+enum Owner {
+    case noOne
+    case player1
+    case player2
+}
