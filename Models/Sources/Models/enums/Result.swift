1d43a80e4ec16776f56418365a68872724b3993c
Nico <nicolas.franco@etu.uca.fr>
2024-01-22 09:38:30 +0100

    Result and WinningReason enums

---
+++ Models/Sources/Models/enums/Result.swift
@@ -0,0 +1,14 @@
+//
+//  Result.swift
+//  
+//
+//  Created by étudiant on 22/01/2024.
+//
+
+import Foundation
+
+public enum Result {
+    case notFinished
+    case even
+    case winner(winningReason: WinningReason)
+}
---
+++ Models/Sources/Models/enums/WinningReason.swift
@@ -0,0 +1,15 @@
+//
+//  WinningReason.swift
+//  
+//
+//  Created by étudiant on 22/01/2024.
+//
+
+import Foundation
+
+public enum WinningReason {
+    case denReached
+    case noMorePieces
+    case noMovesLeft
+    case tooManyOcurrences
+}
