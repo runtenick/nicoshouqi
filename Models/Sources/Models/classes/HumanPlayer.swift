b13ad5e0e38517a9e0821045ec8a4c9ff61a24da
Nico <nicolas.franco@etu.uca.fr>
2024-01-30 17:18:38 +0100

    :wrench: fixed some logic + cli tests :test_tube:

--- Models/Sources/Models/classes/HumanPlayer.swift
+++ Models/Sources/Models/classes/HumanPlayer.swift
@@ -25,8 +25,19 @@ public class HumanPlayer: Player {
     /// - Parameters:
     ///   - board: The game board.
     ///   - rules: The game rules.
-    /// - Returns: The chosen move, or `nil` if no move is chosen.
+    /// - Returns: The chosen move, or `nil` if the move is not valid.
     public override func chooseMove(in board: Board, with rules: Rules) -> Move? {
-        return nil
+        let move = self.inputMethod()
+        
+        do {
+            let isChosenMoveValid = try rules.isMoveValid(board: board, move: move)
+            return move
+            
+        } catch {
+            // Invalid move, for now return nil.
+            // Maybe do error propagation in the future depending on how the game loop
+            // will be handled.
+            return nil
+        }
     }
 }
